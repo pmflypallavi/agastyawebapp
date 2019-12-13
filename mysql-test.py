60f60520efc79da43fe4d3b5e09e251d1be6a935
pmflypallavi <58667171+pmflypallavi@users.noreply.github.com>
2019-12-13 12:09:02 +0530

    Update mysql-test.py

--- mysql-test.py
+++ mysql-test.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
-import MySQLdb as my
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'custom-mysql.gamification.svc.cluster.local'
 app.config['MYSQL_USER'] = 'xxuser1'
@@ -17,10 +16,8 @@ def employees():
             userDetails = cur.fetchall()
             return render_template('employee.html',userDetails=userDetails)
         cur.close()
-    except my.Error as e:
-        print(e)
-    except :
-        print("Unknown error occurred")
+    except Exception as e:
+        return(str(e))
 
 if __name__ == "__main__":
     app.run()
