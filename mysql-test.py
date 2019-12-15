4606082ba7119e15059d3261467493e96bcba3d6
pmflypallavi <58667171+pmflypallavi@users.noreply.github.com>
2019-12-15 16:43:16 +0530

    Update mysql-test.py

--- mysql-test.py
+++ mysql-test.py
@@ -17,7 +17,7 @@ def employees():
             return render_template('employee.html',userDetails=userDetails)
         cur.close()
     except Exception as e:
-        return(str(e))
+        print(str(e))
 
 if __name__ == "__main__":
     app.run()
