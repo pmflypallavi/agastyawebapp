f5d7a993f28a7cada610477516633c4c9e57d234
pmflypallavi <58667171+pmflypallavi@users.noreply.github.com>
2019-12-31 01:18:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,40 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
 application = Flask(__name__)
+
 application.config['MYSQL_HOST'] = 'custom-mysql.gamification.svc.cluster.local'
 application.config['MYSQL_USER'] = 'xxuser'
 application.config['MYSQL_PASSWORD'] = 'welcome1'
 application.config['MYSQL_DB']= 'sampledb'
 mysql = MySQL(application)
+
 @application.route('/')
-def employees():
+def home():
+    return render_template("home.html")
+
+@application.route("/about", methods=['GET', 'POST'])
+def about():
     try:
         cur = mysql.connection.cursor()
-        print(cur)
-        res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM sampledb.XXIBM_PRODUCT_STYLE LIMIT 10")
-        print(res)
+        res = cur.execute("SELECT DESCRIPTION FROM sampledb.XXIBM_PRODUCT_STYLE LIMIT 10")
         if res > 0:
+
             userDetails = cur.fetchall()
-            return render_template('employee.html',userDetails=userDetails)
-        cur.close()
+            product = list()
+            Listprice = list()
+            for i in range(len(userDetails)):
+                product.append(userDetails[i][0])
+            if request.method == 'POST':
+                description = request.form.get('userDetails')
+                price = cur.execute("select List_Price from sampledb.XXIBM_PRODUCT_PRICING where Item_Number in (select Item_Number  from sampledb.XXIBM_PRODUCT_SKU where Description=%s) and In_stock ='Yes' order by Price_Effective_Date desc limit 1",(description,))
+                if price > 0:
+                    Price = cur.fetchall()
+                    Listprice = Price[0][0]
+
+            return render_template('about.html', userDetails=product, Listprice=Listprice)
     except Exception as e:
         return str(e)
+
 
 if __name__ == "__main__":
     application.run()
